5644b1037c1f0ad06743716b12052436b960064f
Leonardo Ramos <leo.ramos.ari@gmail.com>
2021-01-15 11:20:44 -0500

    second

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import './styles/about.css'
-import alexia from './../images/about-back.png'
-import alexiaMobile from './../images/about-mobile.png'
 
  const Home = () => {
   return (
